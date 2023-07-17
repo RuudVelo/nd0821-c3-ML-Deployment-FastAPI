080cf4695da242c9c9fa1bf142eebf44cb07ef54
Ruud Goorden <ruudgoorden@ruuds-mbp.home>
2023-07-17 18:13:07 +0200

    change workflow

--- api_sample.py
+++ api_sample.py
@@ -6,20 +6,19 @@ url = "https://ml-udacity-salary-app-743b155e693f.herokuapp.com/inference/"
 
 # sample for prediction
 example =  {"age": 42,
-        "workclass": "Private",
-        "fnlgt": 159449,
-        "education": "Bachelors",
-        "education-num": 13,
-        "marital-status": "Married-civ-spouse",
-        "occupation": "Exec-managerial",
-        "relationship": "Husband",
-        "race": "White",
-        "sex": "Male",
-        "capital-gain": 5178,
-        "capital-loss": 0,
-        "hours-per-week": 40,
-        "native-country": "United-States"
-        }
+            "workclass": "Private",
+            "fnlgt": 159449,
+            "education": "Bachelors",
+            "education-num": 13,
+            "marital-status": "Married-civ-spouse",
+            "occupation": "Exec-managerial",
+            "relationship": "Husband",
+            "race": "White",
+            "sex": "Male",
+            "capital-gain": 5178,
+            "capital-loss": 0,
+            "hours-per-week": 40,
+            "native-country": "United-States"}
 
 data = json.dumps(example)
 
@@ -28,6 +27,6 @@ response = requests.post(url, data=example )
 
 # Show the response along with example metadata
 print("Response status code", response.status_code)
-print("Predicted salary",response.json()['prediction'])
+#print("Predicted salary",response.json()['prediction'])
 print("response total:")
 print(response.json())
