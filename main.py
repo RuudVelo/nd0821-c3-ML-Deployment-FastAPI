c29163cb9d922d97c4a654c8667b1fe0e327928b
Ruud Goorden <ruudgoorden@ruuds-mbp.home>
2023-07-17 17:20:15 +0200

    added live_get

--- main.py
+++ main.py
@@ -82,5 +82,5 @@ async def predict(inference: InputData):
     return dict_input
 
 
-if __name__ == "__main__":
-    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
+#if __name__ == "__main__":
+#    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
